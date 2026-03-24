ea3187aa065d895808ddf57ae8b49ac28853b745
Daniil <134610042+koksler@users.noreply.github.com>
2026-03-24 21:57:35 +0300

    fix: Bench revamp to support new compos for preview

--- impala-app/src/App.tsx
+++ impala-app/src/App.tsx
@@ -1,9 +1,9 @@
 import { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF, Environment, ContactShadows } from '@react-three/drei';
-// import { Button } from './components/ui/buttons/buttons';
-// import {ImportIcon} from './components/icons/index';
 import { Header } from './components/header';
+import { ObjectSettingsPanel } from './components/ui/menus/objectSettingsPanel'
+import { SceneSettingsPanel } from './components/ui/menus/sceneSettingsPanel';
 
 function PreloadedModel() {
   const { scene } = useGLTF('/model.glb');
@@ -12,34 +12,54 @@ function PreloadedModel() {
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState<'project' | 'home'>('project');
+  const [panelsMinimized, setPanelsMinimized] = useState(false); // Change it to useStore later, sometime. :D
 
   return (
-    <div className="w-full h-full relative bg-white">
-      <Header 
-        variant={currentPage} 
-        projectName="teto_car_scene" 
-        serverStatus="online" 
-      />
-      <div className="absolute top-0 left-0 w-full h-full pointer-events-none p-6 flex flex-col justify-between z-10">
+    <div className="w-screen h-screen overflow-hidden flex flex-col bg-white">
 
-      <button 
-        onClick={() => setCurrentPage(prev => prev === 'project' ? 'home' : 'project')}
-        className="absolute bottom-4 left-4 z-50 bg-bg-item p-2 rounded shadow pointer-events-auto"
-      >
-        Toggle Header Variant
-      </button>
+        <div className="flex-none w-full z-50">
+        <Header 
+          variant={currentPage} 
+          projectName="teto_car_scene" 
+          serverStatus="online" 
+        />
+        </div>
+        
+        <div className="relative flex-1 w-full">
+        
+        {/* UI Overlay Layer */}
+        <div className="absolute inset-0 pointer-events-none z-40 p-4 flex justify-between">
+          
+          {/* Position the panel top-left with auto-pointer events */}
+          <div className="pointer-events-auto h-full">
+            <ObjectSettingsPanel 
+                isMinimized={panelsMinimized} 
+                onToggleMinimize={() => setPanelsMinimized(!panelsMinimized)} 
+            />
+          </div>
 
+          <div className="pointer-events-auto h-full">
+            <SceneSettingsPanel isMinimized={panelsMinimized} />
+          </div>
+
+          <button 
+            onClick={() => setCurrentPage(prev => prev === 'project' ? 'home' : 'project')}
+            className="absolute bottom-4 right-4 z-50 bg-bg-item p-2 rounded shadow pointer-events-auto font-sans text-[12px]"
+          >
+            Toggle Header Variant
+          </button>
+        </div>
+
+        <Canvas className="w-full h-full absolute inset-0" camera={{ position:[3, 2, 5], fov: 45 }}>
+          <color attach="background" args={['white']} />
+          <Suspense fallback={null}>
+            <PreloadedModel />
+            <Environment preset="city" />
+            <ContactShadows position={[0, -0.5, 0]} opacity={0.6} scale={10} blur={2} far={4} />
+          </Suspense>
+          <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2 + 0.1} />
+        </Canvas>
       </div>
-
-      <Canvas camera={{ position:[3, 2, 5], fov: 45 }}>
-        <color attach="background" args={['white']} />
-        <Suspense fallback={null}>
-          <PreloadedModel />
-          <Environment preset="city" />
-          <ContactShadows position={[0, -0.5, 0]} opacity={0.6} scale={10} blur={2} far={4} />
-        </Suspense>
-        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2 + 0.1} />
-      </Canvas>
     </div>
   );
 }
